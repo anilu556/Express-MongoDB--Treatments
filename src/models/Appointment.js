b0887cebe4b50dc77d2b36d97e32ad522eaec288
anilu556 <anilu556@hotmail.com>
2019-01-29 12:26:24 -0600

    sprint 2, models

---
+++ src/models/Appointment.js
@@ -0,0 +1,13 @@
+const moongose = require('mongoose')
+const {Schema} = mongoose
+
+const appointmentSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  name: { type: String, required: true},
+  phone_number: { type: Number, required: true},
+  day: { type: String, required: true},
+  treatment_id: { type: Schema.Types.ObjectId, ref: 'Treatment'},
+  user: {type: Schema.Types.ObjectId, ref: 'User'}
+})
+
+module.exports = mongoose.model('Appointment', appointmentSchema)
