0a61979b591dd8569ad4bd6826f999e5bd684833
anilu556 <anilu556@hotmail.com>
2019-01-29 12:57:21 -0600

    sprint3, routes

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,4 +1,33 @@
 const { Router } = require('express');
 const app = Router();
 
+const Users = require('../controllers/users/users')
+const Appointments = require('../controllers/treatments/treatments')
+const Appointments = require('../controllers/treatments/appointments')
+
+//Users routes
+
+app.get('/users', Users.index);
+app.get('/users/:userId', Users.findBy);
+app.get('/users/:userId/treatments', Users.findTreatmentsBy)
+app.post('/users', Users.create);
+app.put('/users/:userId', Users.updateBy);
+//app.delete('/users/:userId', Users.removeBy);
+
+//Treatments routes
+
+app.get('/treatments', Treatments.index);
+app.get('/treatments/:treatmentId', Treatments.findBy);
+app.get('/treatments/:treatmentId/appointments', Users.findAppointmentsBy)
+app.post('/treatments', Treatments.create);
+app.put('/treatments/:id', Treatments.updateBy);
+//app.delete('/treatments/:treatmentId', Treatments.removeBy);
+
+//Appointments routes
+
+app.get('/appointments', Appointments.index);
+app.get('/appointments/:appointmenstId', Appointments.findBy);
+// app.put('/appointments/:id', Appointments.updateBy);
+// app.delete('/appointments/:appointmentsId', Appointments.removeBy);
+
 module.exports = app;
